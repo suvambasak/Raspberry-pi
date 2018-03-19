a4477278bd3cf4ec27ad233b6bfb9a672f602f24
Suvam Basak <suvambasak2008@gmail.com>
2018-03-19 13:47:34 +0530

    Test

--- TakePicture.py
+++ TakePicture.py
@@ -4,11 +4,11 @@ import os
 camera = picamera.PiCamera()
 camera.capture('example.jpg')
 
-
-
 if not os.path.exists('flip'):
 	os.makedirs('flip')
 
 camera.vflip = True
+
+# print ('Fliped')
 
 camera.capture('flip/example2.jpg')
